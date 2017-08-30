dcbfa1184e19678f94fa3cf2fbcc1aed94dfc0e9
Salim Chemes <salim.chemes@alaskaair.com>
2017-08-30 05:46:57 -0700

    adding env

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('checkPage');
+    var env = document.getElementById('env');
+    env.innerHTML = "Environment: LocalHost";
     checkPageButton.addEventListener('click', function() { 
     console.log("ASCOM chrome extension")
     }, false);
